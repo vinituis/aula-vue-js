877e1a33b9ddab5088125baa0fed3d91817daea8
Vinicius Fernandes <vinicius.turu@gmail.com>
2022-01-19 08:01:53 -0300

    first commit

---
+++ vuejs/1_install/src/app.js
@@ -0,0 +1,19 @@
+const MyNameApp = {
+    data() {
+        return{
+            name: "",
+            idade: 23,
+            input_name: ""
+        }
+    },
+    methods: {
+        submitForm(e) {
+            e.preventDefault();
+
+            console.log(this.input_name);
+            this.name = this.input_name;
+        }
+    }
+}
+
+Vue.createApp(MyNameApp).mount("#app");
