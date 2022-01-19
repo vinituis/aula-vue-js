849c7f3627062b97958d2bd245c0f2fba23b612d
Vinicius Fernandes <vinicius.turu@gmail.com>
2022-01-19 08:17:09 -0300

    aula 3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <primeiro-componente />
+    <p>Essa aplicação é: {{ testando }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import primeiroComponente from './components/primeiroComponente.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      primeiroComponente
+    },
+    data() {
+      return {
+        testando: 'teste'
+      }
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/primeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho com {{ profissao }}.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'primeiroComponente',
+        data() {
+            return {
+                nome: 'Vinicius',
+                profissao: 'programador'
+            }
+        }
+    }
+</script>
